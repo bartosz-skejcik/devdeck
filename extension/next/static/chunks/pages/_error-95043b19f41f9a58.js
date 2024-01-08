6e460350cdd032340f1d45a59081f64da3a37837
Bartek Paczesny <bartek@paczesny.pl>
2024-01-08 23:08:59 +0100

    Add extension build

---
+++ extension/next/static/chunks/pages/_error-95043b19f41f9a58.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{4098:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(8253)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4098)}),_N_E=n.O()}]);
