b048415848d1b263f0df0548072cf7b1fa38bc30
Bartek Paczesny <bartek@paczesny.pl>
2024-01-09 18:46:03 +0100

    New build with fixes

---
+++ extension/next/static/jBOIFGwYgNqVDjaSy70RE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/618-ec3f0a290c8c64f4.js","static/chunks/pages/index-19d21bcac47d3684.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- extension/next/static/zTxhdId9bk2LGx0Qfv1Be/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/618-ec3f0a290c8c64f4.js","static/chunks/pages/index-3616ba91f3a6d515.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
