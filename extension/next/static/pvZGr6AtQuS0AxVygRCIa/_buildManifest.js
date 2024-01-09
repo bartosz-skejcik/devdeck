35bf24d6652662ebccccecd162817a01ed05353f
Bartek Paczesny <bartek@paczesny.pl>
2024-01-09 16:38:37 +0100

    New build with spotify

--- extension/next/static/pvZGr6AtQuS0AxVygRCIa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/665-e554df3dabc0cbb4.js","static/chunks/pages/index-6b35434dfecc7ebd.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
