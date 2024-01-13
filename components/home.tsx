07357b3687572edc762c06aaaf5faadcb8a62b7d
Bartek Paczesny <bartek@paczesny.pl>
2024-01-13 22:14:57 +0100

    fix: fetching articles with tags

--- components/home.tsx
+++ components/home.tsx
@@ -3,11 +3,14 @@ import useArticles from "@/hooks/use-articles";
 import SpotifyWidget from "@/components/spotify/widget";
 import WeatherWidget from "@/components/weather/widget";
 import ClockWidget from "@/components/clock/widget";
+import useStore from "@/hooks/use-store";
+import { useUserPreferences } from "@/stores/user-preferences";
 
 type Props = {};
 
 function Home({}: Props) {
-    const { articles, loading } = useArticles();
+    const tags = useStore(useUserPreferences, (state) => state.filterTags);
+    const { articles, loading } = useArticles(tags);
     return (
         <main className="grid w-full h-full grid-cols-8 grid-rows-6 gap-3 overflow-y-auto 2xl:gap-5 grow">
             <WeatherWidget />
