d4cac549cbb6a1f55092c20299ccd099abb0957d
Bartek Paczesny <bartek@paczesny.pl>
2024-01-14 19:11:18 +0100

    refactor: text wrapping and spacing

--- components/articles/widget.tsx
+++ components/articles/widget.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 import { Article } from "@/types/article";
@@ -19,12 +18,16 @@ function ArticlesWidget({ articles }: Props) {
     }, [articles]);
 
     return (
-        <Card className="flex items-start justify-center col-span-3 row-span-5 py-2 2xl:row-span-3 2xl:col-span-2">
-            <CardContent className="w-full space-y-2">
-                <h3 className="text-lg font-semibold">Most Recent Articles</h3>
-                {mostRecentArticles.map((article) => (
-                    <Article key={article.id} article={article} />
-                ))}
+        <Card className="flex items-center justify-center col-span-3 row-span-5 py-2 2xl:row-span-3 2xl:col-span-2">
+            <CardContent className="flex flex-col items-start justify-between w-full h-full">
+                <h3 className="mb-1 text-lg font-semibold">
+                    Most Recent Articles
+                </h3>
+                <div className="flex w-full flex-col items-center justify-center space-y-[1.35vh]">
+                    {mostRecentArticles.map((article) => (
+                        <Article key={article.id} article={article} />
+                    ))}
+                </div>
             </CardContent>
         </Card>
     );
@@ -72,7 +75,7 @@ const Article = ({ article }: { article: Article }) => {
                         {article.title.slice(0, 35) + "..."}
                     </h4>
                     <p className="text-sm text-gray-500 dark:text-gray-400 line-clamp-2">
-                        {article.description.slice(0, 50)}
+                        {article.description.slice(0, 45) + "..."}
                     </p>
                 </div>
             </div>
