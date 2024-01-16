d16d2bbed97064f47926c6f54652c3068060211e
Bartek Paczesny <bartek@paczesny.pl>
2024-01-16 01:46:18 +0100

    New build - ux optimizations

---
+++ extension/next/static/C-4i3oIYE-GSIeExZ0lRO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/40-b43663133baef59c.js","static/chunks/pages/index-e550d6cf69234252.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
