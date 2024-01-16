c8e7859fc8d57ced872a6193102486626fe43669
Bartek Paczesny <bartek@paczesny.pl>
2024-01-16 14:54:15 +0100

    fix: progressbar colors not showing

--- components/atlassian/hall-of-fame/progress-bar.tsx
+++ components/atlassian/hall-of-fame/progress-bar.tsx
@@ -6,18 +6,64 @@ type Props = {
     color: string;
 };
 
+const colors = [
+    {
+        name: "yellow",
+        hex: "#ca8a04",
+        value: 600,
+    },
+    {
+        name: "yellow",
+        hex: "#eab308",
+        value: 500,
+    },
+    {
+        name: "green",
+        hex: "#65a30d",
+        value: 600,
+    },
+    {
+        name: "green",
+        hex: "#84cc16",
+        value: 500,
+    },
+    {
+        name: "blue",
+        hex: "#0284c7",
+        value: 600,
+    },
+    {
+        name: "blue",
+        hex: "#0ea5e9",
+        value: 500,
+    },
+];
+
+const calculatedColorGradientClassName = (color: string) => {
+    switch (color) {
+        case "yellow":
+            return "bg-gradient-to-r from-yellow-500 to-yellow-400";
+        case "green":
+            return "bg-gradient-to-r from-green-500 to-green-400";
+        case "blue":
+            return "bg-gradient-to-r from-sky-500 to-sky-400";
+        default:
+            return "";
+    }
+};
+
 function ProgressBar({ progress, name, color }: Props) {
     return (
         <div className="flex flex-col items-start justify-start w-full gap-2">
-            <p className="font-medium text-md">{name}</p>
+            <p className="text-base font-medium">{name}</p>
             <div className="flex items-center w-full gap-2">
                 <div className="w-full h-2 rounded-full bg-muted">
                     <div
                         className={clsx(
                             "h-2 rounded-full",
-                            `bg-gradient-to-r from-${
+                            calculatedColorGradientClassName(
                                 color.split("-")[0]
-                            }-600 to-${color.split("-")[0]}-500`
+                            )
                         )}
                         style={{ width: `${progress}%` }}
                     ></div>
